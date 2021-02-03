257df5cdd4187c00e57f0cc8f112b6c96e432254
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-03 10:30:48 +0200

    add some checks into Fibonaci decision

--- Program.cs
+++ Program.cs
@@ -7,39 +7,48 @@ namespace ArrayPrintingFibonacci
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a positive number > 0");
+
             int nElement = int.Parse(Console.ReadLine()); //read the n element
-            int[] arrFibonaci = new int[nElement]; // create an array with nElements
 
-            //We have to fill the array to the last -> n element, how to do this, We know the value of index 1 and index 2]
-            int length = arrFibonaci.Length;
-
-            if (length == 1)
+            if (nElement <= 0)
             {
-                arrFibonaci[0] = 1;
-               // Console.WriteLine(arrFibonaci[0]);
+                Console.WriteLine("Dear User , please enter a positive number > 0");
             }
-
-            if (length == 2)
+            else 
             {
-                arrFibonaci[1] = 1;
-               // Console.WriteLine(arrFibonaci[]);
-            }            
-           
-            if (length > 2)
-            {
-                arrFibonaci[0] = 1;
-                arrFibonaci[1] = 1;
+                int[] arrFibonaci = new int[nElement]; // create an array with nElements
 
-                for (int i = 2; i < arrFibonaci.Length; i++)
-                {   
-                   arrFibonaci[i] = arrFibonaci[i - 1] + arrFibonaci[i - 2]; //We fill the arrFibonaci with numbers,till last desired element
-                    // Console.WriteLine(arrFibonaci[i]); // We print every filled element here
+                //We have to fill the array to the last -> n element, how to do this, We know the value of index 1 and index 2]
+                int length = arrFibonaci.Length;
+
+                if (length == 1)
+                {
+                    arrFibonaci[0] = 1;
+                    // Console.WriteLine(arrFibonaci[0]);
                 }
+
+                if (length == 2)
+                {
+                    arrFibonaci[1] = 1;
+                    // Console.WriteLine(arrFibonaci[]);
+                }
+
+                if (length > 2)
+                {
+                    arrFibonaci[0] = 1;
+                    arrFibonaci[1] = 1;
+
+                    for (int i = 2; i < arrFibonaci.Length; i++)
+                    {
+                        arrFibonaci[i] = arrFibonaci[i - 1] + arrFibonaci[i - 2]; //We fill the arrFibonaci with numbers,till last desired element
+                                                                                  // Console.WriteLine(arrFibonaci[i]); // We print every filled element here
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine(arrFibonaci[arrFibonaci.Length - 1]); // We print the last Element , so desired by the client
             }
-            
-            Console.WriteLine();
-            Console.WriteLine(arrFibonaci[arrFibonaci.Length-1]); // We print the last Element , so desired by the client
-            
         }
     }
 }
